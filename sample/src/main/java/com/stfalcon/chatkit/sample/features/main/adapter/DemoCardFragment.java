d1312241d023c86e33c430df1152502cd7e7cb3e
troy379 <krol.oleksandr@stfalcon.com>
2017-04-11 11:30:12 +0300

    sample main screen update

---
+++ sample/src/main/java/com/stfalcon/chatkit/sample/features/main/adapter/DemoCardFragment.java
@@ -0,0 +1,103 @@
+package com.stfalcon.chatkit.sample.features.main.adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.stfalcon.chatkit.sample.R;
+
+/*
+ * Created by troy379 on 11.04.17.
+ */
+public class DemoCardFragment extends Fragment
+        implements View.OnClickListener {
+
+    private static final String ARG_ID = "id";
+    private static final String ARG_TITLE = "title";
+    private static final String ARG_DESCRIPTION = "description";
+
+    private int id;
+    private String title, description;
+    private OnActionListener actionListener;
+
+    public DemoCardFragment() {
+
+    }
+
+    public static DemoCardFragment newInstance(int id, String title, String description) {
+        DemoCardFragment fragment = new DemoCardFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_ID, id);
+        args.putString(ARG_TITLE, title);
+        args.putString(ARG_DESCRIPTION, description);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.id = getArguments().getInt(ARG_ID);
+            this.title = getArguments().getString(ARG_TITLE);
+            this.description = getArguments().getString(ARG_DESCRIPTION);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_demo_card, container, false);
+
+        TextView tvTitle = (TextView) v.findViewById(R.id.tvTitle);
+        TextView tvDescription = (TextView) v.findViewById(R.id.tvDescription);
+        Button button = (Button) v.findViewById(R.id.button);
+
+        tvTitle.setText(title);
+        tvDescription.setText(description);
+        button.setOnClickListener(this);
+
+        return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                onAction();
+                break;
+        }
+    }
+
+    public void onAction() {
+        if (actionListener != null) {
+            actionListener.onAction(this.id);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnActionListener) {
+            actionListener = (OnActionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnActionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        actionListener = null;
+    }
+
+    public interface OnActionListener {
+        void onAction(int id);
+    }
+}
---
+++ sample/src/main/java/com/stfalcon/chatkit/sample/features/main/adapter/MainActivityPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.stfalcon.chatkit.sample.features.main.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.stfalcon.chatkit.sample.R;
+
+/*
+ * Created by troy379 on 11.04.17.
+ */
+public class MainActivityPagerAdapter extends FragmentStatePagerAdapter {
+
+    public static final int ID_DEFAULT = 0;
+    public static final int ID_STYLED = 1;
+    public static final int ID_CUSTOM_LAYOUT = 2;
+    public static final int ID_CUSTOM_VIEW_HOLDER = 3;
+    public static final int ID_CUSTOM_CONTENT = 4;
+
+    private Context context;
+
+    public MainActivityPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        String title = null;
+        String description = null;
+        switch (position) {
+            case ID_DEFAULT:
+                title = context.getString(R.string.sample_title_default);
+                description = context.getString(R.string.sample_subtitle_default);
+                break;
+            case ID_STYLED:
+                title = context.getString(R.string.sample_title_attrs);
+                description = context.getString(R.string.sample_subtitle_attrs);
+                break;
+            case ID_CUSTOM_LAYOUT:
+                title = context.getString(R.string.sample_title_layout);
+                description = context.getString(R.string.sample_subtitle_layout);
+                break;
+            case ID_CUSTOM_VIEW_HOLDER:
+                title = context.getString(R.string.sample_title_holder);
+                description = context.getString(R.string.sample_subtitle_holder);
+                break;
+            case ID_CUSTOM_CONTENT:
+                title = context.getString(R.string.sample_title_custom_content);
+                description = context.getString(R.string.sample_subtitle_custom_content);
+                break;
+        }
+        return DemoCardFragment.newInstance(position, title, description);
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}

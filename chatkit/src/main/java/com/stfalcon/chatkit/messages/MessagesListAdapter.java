91fd6997c2cb51e7a91628a0096e7c4a79e6e63d
Anton <anton.bevza@stfalcon.com>
2018-07-10 12:38:22 +0300

    hot fix

--- chatkit/src/main/java/com/stfalcon/chatkit/messages/MessagesListAdapter.java
+++ chatkit/src/main/java/com/stfalcon/chatkit/messages/MessagesListAdapter.java
@@ -157,7 +157,7 @@ public class MessagesListAdapter<MESSAGE extends IMessage>
      * @param reverse  {@code true} if need to reverse messages before adding.
      */
     public void addToEnd(List<MESSAGE> messages, boolean reverse) {
-        if (messages.isEmpty()) return
+        if (messages.isEmpty()) return;
     
         if (reverse) Collections.reverse(messages);
 
